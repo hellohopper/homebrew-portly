2fef3a710e3c00690cbe254454a50487341bdf42
hellohopper <258135505+hellohopper@users.noreply.github.com>
2026-07-01 01:43:14 -0400

    Update portly cask to v0.2.0

--- Casks/portly.rb
+++ Casks/portly.rb
@@ -1,6 +1,6 @@
 cask "portly" do
-  version "0.1.0"
-  sha256 "bba112364db81198fbbd506c0bad35869634eeff558d5e126f1109ffd1db1cc0"
+  version "0.2.0"
+  sha256 "47e6164b4338b671f9ba6adf579cbfd7544778df4df0fe4d644e6c3ffaf2546d"
 
   url "https://github.com/hellohopper/portly/releases/download/v#{version}/Portly.dmg"
   name "Portly"
